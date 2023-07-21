5b8e39d78884942429af06d9eb7575fc5d5e7d3f
Yang Luo <hsluoyz@qq.com>
2023-07-21 23:05:26 +0800

    Remember login session, fix other bugs

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:casdoor_flutter_sdk/casdoor.dart';
 import 'package:casdoor_flutter_sdk/casdoor_flutter_sdk_config.dart';
 import 'package:casdoor_flutter_sdk/casdoor_flutter_sdk_platform_interface.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -18,45 +19,80 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _token = 'User is not logged in';
+  String _token = '';
 
   final AuthConfig _config = AuthConfig(
-      clientId: "014ae4bd048734ca2dea",
-      serverUrl: "https://door.casdoor.com",
-      organizationName: "casbin",
-      appName: "app-casnode",
-      redirectUri: "http://localhost:9000/callback.html",
-      callbackUrlScheme: "casdoor");
+      clientId: '014ae4bd048734ca2dea',
+      serverUrl: 'https://door.casdoor.com',
+      organizationName: 'casbin',
+      appName: 'app-casnode',
+      redirectUri: 'http://localhost:9000/callback.html',
+      callbackUrlScheme: 'casdoor');
 
   @override
   void initState() {
     super.initState();
+    restoreToken();
+  }
+
+  Future<void> restoreToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('access_token');
+
+    if (token != null) {
+      setState(() {
+        _token = token;
+      });
+    }
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> authenticate() async {
+  Future<void> login() async {
     // Get platform information
     final platform =
-        await CasdoorFlutterSdkPlatform.instance.getPlatformVersion() ?? "";
+        await CasdoorFlutterSdkPlatform.instance.getPlatformVersion() ?? '';
     String callbackUri;
-    if (platform == "web") {
-      callbackUri = "${_config.redirectUri}";
+    if (platform == 'web') {
+      callbackUri = '${_config.redirectUri}';
     } else {
-      callbackUri = "${_config.callbackUrlScheme}://callback";
+      callbackUri = '${_config.callbackUrlScheme}://callback';
     }
     _config.redirectUri = callbackUri;
     final Casdoor _casdoor = Casdoor(config: _config);
     final result = await _casdoor.show();
     // Get code
-    final code = Uri.parse(result).queryParameters['code'] ?? "";
+    final code = Uri.parse(result).queryParameters['code'] ?? '';
     final response = await _casdoor.requestOauthAccessToken(code);
+    final token = jsonDecode(response.body)['access_token'] as String;
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('access_token', token);
+
     setState(() {
-      _token = jsonDecode(response.body)["access_token"] as String;
+      _token = token;
+    });
+  }
+
+  Future<void> logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('access_token', '');
+
+    setState(() {
+      _token = '';
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final Casdoor _casdoor = Casdoor(config: _config);
+    String userName = "";
+    String userAvatar = "";
+    if (_token != '') {
+      Map<String, dynamic> map = _casdoor.decodedToken(_token);
+      userName = map['name'];
+      userAvatar = map['avatar'];
+    }
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
@@ -66,10 +102,23 @@ class _MyAppState extends State<MyApp> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('Current user: $_token\n'),
+                Text(_token == '' ? 'Current user: (empty)\n' : 'Current user:\n'),
+                Visibility(
+                  visible: _token != '',
+                  child:
+                  Column(
+                    children: [
+                      Image.network(
+                        _token == '' ? '' : userAvatar,
+                        width: 100,
+                        height: 100,
+                      ),
+                      Text(_token == '' ? '' : userName),
+                    ],
+                  ),
+                ),
                 ElevatedButton(
-                  onPressed: authenticate,
-                  child: const Text('Login'),
+                  onPressed: (_token == '') ? login : logout,
                   style: ButtonStyle(
                     padding: MaterialStateProperty.all<EdgeInsets>(
                       EdgeInsets.all(20),
@@ -78,6 +127,7 @@ class _MyAppState extends State<MyApp> {
                       Size(200, 50),
                     ),
                   ),
+                  child: Text((_token == '') ? 'Login' : 'Logout'),
                 ),
               ],
             ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import casdoor_flutter_sdk
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   CasdoorFlutterSdkPlugin.register(with: registry.registrar(forPlugin: "CasdoorFlutterSdkPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
